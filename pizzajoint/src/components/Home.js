8539a80dcb3566617ae4da6fc2e99684e8293878
daraksha-dtg <daraksha.atg@gmail.com>
2022-02-22 21:44:42 +0530

    code refactor

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,19 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  // visible: {
+  //   x: [0, -20, 20, -20, 20, 0],
+  //   transition: {
+  //     delay: 2,
+  //   },
+  // },
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 1.5,
+    },
+    exit: {
+      x: "-100vw",
+      transition: { ease: "easeInOut" },
+    },
+  },
+};
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div className="home container" animate={{}}>
+      <motion.h2
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        // transition={{ delay: 1.5, duration: 1.5 }}
+        exit="exit"
+      >
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          variants={buttonVariants}
+          // animate="visible"
+          whileHover="hover"
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
---
+++ pizzajoint/src/components/Modal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+
+const backdrop = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdrop}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.div className="modal">
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
